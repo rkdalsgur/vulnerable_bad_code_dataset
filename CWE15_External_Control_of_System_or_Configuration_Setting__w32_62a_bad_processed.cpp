78a0272bfc8182c630baf05c74d6ad3a8b55a142
rkdalsgur <127273488+rkdalsgur@users.noreply.github.com>
2025-09-08 14:04:48 +0900

    6th

--- CWE15_External_Control_of_System_or_Configuration_Setting__w32_62a_bad_processed.cpp
+++ CWE15_External_Control_of_System_or_Configuration_Setting__w32_62a_bad_processed.cpp
@@ -10,7 +10,7 @@ namespace CWE15_External_Control_of_System_or_Configuration_Setting__w32_62 {
 
 /* bad function declaration */
 void badSource(char * &data);
-
+/*123*/
 void bad() {
  char * data;
  char dataBuffer[100] = "";
